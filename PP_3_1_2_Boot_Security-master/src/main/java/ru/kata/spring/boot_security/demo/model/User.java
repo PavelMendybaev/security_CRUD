d4e19529626bb0ce9ab3730d0326abcebd9b91b7
PavelMendybaev <72722409+PavelMendybaev@users.noreply.github.com>
2022-02-22 19:52:20 +0500

    Add files via upload

---
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -0,0 +1,44 @@
+package ru.kata.spring.boot_security.demo.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+
+
+    public User(String name) {
+        this.name = name;
+
+    }
+    public User(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
