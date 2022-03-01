532c5c6e45a9a00e854c818c0bb0c001d8b4b682
вадим гаврилов <df_vadim_dfg@mail.ru>
2022-03-01 12:53:48 +0500

    version 6 final_authority

--- PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controller/MainController.java
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controller/MainController.java
@@ -2,7 +2,9 @@ package ru.kata.spring.boot_security.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.kata.spring.boot_security.demo.model.Role;
@@ -16,6 +18,9 @@ import java.util.List;
 public class MainController {
 
     private final UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @Autowired
@@ -51,12 +56,20 @@ public class MainController {
 
 
     @RequestMapping(value = "/admin" , method = RequestMethod.GET)
-    public String getAdmin(){
+    public String getAdmin(ModelMap model){
 
         List<User> users = userService.users();
-
+        model.addAttribute("users", users);
         return "admin";
     }
+
+
+    @RequestMapping(value = "/admin" , method = RequestMethod.POST)
+    public String admin(){
+        userService.save(new User("user2" , passwordEncoder.encode("user2") ,Role.USER));
+        return "admin";
+    }
+
 
 
 
